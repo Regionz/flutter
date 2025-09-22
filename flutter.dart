96f8a9547f1246e879f58a2854574fa6816b5cf6
Regionz <artemitdelbin@gmail.com>
2025-09-22 15:52:21 +0300

    Update flutter.dart

--- flutter.dart
+++ flutter.dart
@@ -107,7 +107,7 @@ class _MyHomePageState extends State<MyHomePage> {
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
-        child: const Icon(Icons.arrow_upward),
+        child: const Icon(Icons.visibility),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
